6f187382dfccaa78e5e1c01edca597ed73f32766
Edmund Yap <edmundyjl0505@gmail.com>
2025-04-15 15:31:51 +0800

    Fix: use process.env.PORT fallback to 3000

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use("/api/v1/users", usersRouter);
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 mongoose
   .connect(process.env.MONGO_URI)
